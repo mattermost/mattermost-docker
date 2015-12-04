2d908b3cca955ba28852e77d9e6cc631041b5255
Yi EungJun <eungjun.yi@navercorp.com>
2015-12-04 17:58:43 +0900

    Fix a bug that config.json is not created
    Do not copy config.json into /matteronst/config directly because:
    
    > Changes to a data volume will not be included when you update an image.
    >
    > -- https://docs.docker.com/engine/userguide/dockervolumes/
    
    Instead copy config.template.json into the root and generate
    /mattermost/config/config.json from the file.

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo Starting Platform
-sed -Ei "s/PG_ADDR/$PG_PORT_5432_TCP_ADDR/" /mattermost/config/config.json
-sed -Ei "s/PG_PORT/$PG_PORT_5432_TCP_PORT/" /mattermost/config/config.json
+config=/mattermost/config/config.json
+if [ ! -f $config ]; then
+    cp /config.template.json $config
+    sed -Ei "s/PG_ADDR/$PG_PORT_5432_TCP_ADDR/" $config
+    sed -Ei "s/PG_PORT/$PG_PORT_5432_TCP_PORT/" $config
+fi
 cd /mattermost/bin
 ./platform
