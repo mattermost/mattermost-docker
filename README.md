b4ad7f2119246b36ae8d7f4f9302cc7f6ec8be42
Yi EungJun <eungjun.yi@navercorp.com>
2015-12-09 17:17:25 +0900

    Wait until database is ready

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Remove `volumes` directory
 ## Known Issues
 
 * Do not modify the Listen Address in Service Settings.
-* Sometimes 'app' container fails to start because of "connection refused" to
+* Rarely 'app' container fails to start because of "connection refused" to
   database. Workaround: Restart the container.
 * Sometimes database connection is misconfigured. Workaround: Delete
   `volumes/app/mattermost/config/config.json` and restart the 'app' container.
--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-echo Starting Platform
 config=/mattermost/config/config.json
-if [ ! -f $config ]; then
+echo -ne "Configure database connection..."
+if [ ! -f $config ]
+then
     cp /config.template.json $config
     sed -Ei "s/PG_ADDR/$PG_PORT_5432_TCP_ADDR/" $config
     sed -Ei "s/PG_PORT/$PG_PORT_5432_TCP_PORT/" $config
+    echo OK
+else
+    echo SKIP
 fi
+
+echo -n "Wait until database is ready..."
+until nc -z $PG_PORT_5432_TCP_ADDR $PG_PORT_5432_TCP_PORT
+do
+    echo -n .
+    sleep 1
+done
+
+# Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
+sleep 1
+
+echo "OK"
+
+echo "Starting platform"
 cd /mattermost/bin
 ./platform
