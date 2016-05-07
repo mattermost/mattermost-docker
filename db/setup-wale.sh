97966787dd393cac2b5d249a7f22794e35ec39c6
Pan Luo <pan.luo@ubc.ca>
2016-05-06 23:35:29 -0700

    Use $PGDATA env var to setup wal-e
    $PGDATA is set in upstream postgres dockerfile and should be used in
    downstream in case the postgres data directory is changed in the future.

--- db/setup-wale.sh
+++ db/setup-wale.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # wal-e specific
-echo "wal_level = archive" >> /var/lib/postgresql/data/postgresql.conf
-echo "archive_mode = on" >> /var/lib/postgresql/data/postgresql.conf
-echo "archive_command = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
-echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
+echo "wal_level = $WAL_LEVEL" >> $PGDATA/postgresql.conf
+echo "archive_mode = $ARCHIVE_MODE" >> $PGDATA/postgresql.conf
+echo "archive_command = 'envdir /etc/wal-e.d/env /usr/local/bin/wal-e wal-push %p'" >> $PGDATA/postgresql.conf
+echo "archive_timeout = 60" >> $PGDATA/postgresql.conf
 
 # no cron in the image, use systemd timer on host instead
 #su - postgres -c "crontab -l | { cat; echo \"0 3 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push /var/lib/postgresql/data\"; } | crontab -"
