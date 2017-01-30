4c30235fdefa16f5e9dd19c2a9a6699cf9647d4a
Pan Luo <pan.luo@ubc.ca>
2017-01-30 15:57:17 -0800

    Export vars to fix the postgres container error

--- db/docker-entrypoint1.sh
+++ db/docker-entrypoint1.sh
@@ -4,12 +4,18 @@
 : ${WAL_LEVEL:=minimal}
 : ${ARCHIVE_MODE:=off}
 : ${ARCHIVE_TIMEOUT:=60}
+
+export WAL_LEVEL
+export ARCHIVE_MODE
+export ARCHIVE_TIMEOUT
+
 # PGDATA is defined in upstream postgres dockerfile
 
 function update_conf () {
     if [ -f $PGDATA/postgresql.conf ]; then
         sed -i "s/wal_level =.*$/wal_level = $WAL_LEVEL/g" $PGDATA/postgresql.conf
         sed -i "s/archive_mode =.*$/archive_mode = $ARCHIVE_MODE/g" $PGDATA/postgresql.conf
+        sed -i "s/archive_timeout =.*$/archive_timeout = $ARCHIVE_TIMEOUT/g" $PGDATA/postgresql.conf
     fi
 }
 
