bc9c9e9e2c2e6edf13ce3c8e5f262767f4be55a0
Christoph König <christoph.koenig@kit.edu>
2017-02-06 17:24:09 +0100

    Switch to nginx alpine image

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 echo Starting Nginx
 sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
 sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost-ssl
 if [ "$MATTERMOST_ENABLE_SSL" = true ]; then
     ssl="-ssl"
 fi
-ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/sites-enabled/mattermost
+ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
 nginx -g 'daemon off;'
