73de420141ce1b085997ad9c6c2dca3ea69de74c
Nikolaus <nikolaus@tlt.local>
2017-02-20 09:50:25 +0400

    make use of mainline nginx image, switch ssl/non-ssl config file based on presence of cert/key, hardcode app port since it is hardcoded in the app container as well

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo Starting Nginx
-sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
-sed -Ei "s/APP_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost-ssl
-if [ "$MATTERMOST_ENABLE_SSL" = true ]; then
-    ssl="-ssl"
+if [ -f "/cert/cert.pem" -a -f "/cert/key-no-password.pem" ]; then
+  echo "found certificate and key, linking ssl config"
+  ssl="-ssl"
+else
+  echo "linking plain config"
 fi
-ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/sites-enabled/mattermost
+ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
 nginx -g 'daemon off;'
