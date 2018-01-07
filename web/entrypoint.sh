4822081623f9e87b55f79a3670f7d8d826828653
Ankur Maharjan <akrmhrjn@Ankurs-MacBook-Pro.local>
2018-01-07 16:01:27 +0545

    updated the scripts to generate SSL from letsencrypt

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -5,19 +5,22 @@ APP_HOST=${APP_HOST:-app}
 APP_PORT_NUMBER=${APP_PORT_NUMBER:-80}
 
 # Check if SSL should be enabled (if certificates exists)
-# if [ -f "/cert/cert.pem" -a -f "/cert/key-no-password.pem" ]; then
-#   echo "found certificate and key, linking ssl config"
-#   ssl="-ssl"
-# else
-#   echo "linking plain config"
-# fi
-
+if [ -f "/cert/cert.pem" -a -f "/cert/key-no-password.pem" ]; then
+  echo "found certificate and key, linking ssl config"
+  ssl="-ssl"
+else
+  echo "linking plain config"
+fi
 # Linking Nginx configuration file
 ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
 
 # Setup app host and port on configuration file
+sed -i "s/{%SERVER_NAME%}/${SERVER_NAME}/g" /etc/nginx/conf.d/mattermost.conf
 sed -i "s/{%APP_HOST%}/${APP_HOST}/g" /etc/nginx/conf.d/mattermost.conf
 sed -i "s/{%APP_PORT%}/${APP_PORT_NUMBER}/g" /etc/nginx/conf.d/mattermost.conf
 
 # Run Nginx
-nginx
+nginx 
+
+# get certificate from letsencrypt
+/usr/bin/letsencrypt.sh
--- web/letsencrypt.sh
+++ web/letsencrypt.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+if [ ${LETSENCRYPT_SSL_GENERATION} ]; then
+  echo "Running certificate generation from Letsencrypt."
+  certbot -m ${EMAIL} -d ${DOMAIN_NAME} --agree-tos -n --nginx
 
+  # try to run renew certificate every day
+  echo "@midnight * * * * certbot renew" | crontab
+
+  #run cron
+  cron
+else
+  echo "Not running certificate generation from Letsencrypt."
+fi
