3fa72e438667184690f59ed523cffcf9d4330d78
PICHOU Kyâne <kyane@kyane.fr>
2018-10-21 20:22:40 +0200

    Improve app to db connection setup

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,8 +8,6 @@ generate_salt() {
 # Read environment variables or set default values
 DB_HOST=${DB_HOST:-db}
 DB_PORT_NUMBER=${DB_PORT_NUMBER:-5432}
-MM_USERNAME=${MM_USERNAME:-mmuser}
-MM_PASSWORD=${MM_PASSWORD:-mmuser_password}
 MM_DBNAME=${MM_DBNAME:-mattermost}
 MM_CONFIG=${MM_CONFIG:-/mattermost/config/config.json}
 
@@ -51,20 +49,20 @@ if [ "$1" = 'mattermost' ]; then
     jq '.SqlSettings.DriverName = "postgres"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
     jq '.SqlSettings.AtRestEncryptKey = "'$(generate_salt)'"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
     jq '.PluginSettings.Directory = "/mattermost/plugins/"' $MM_CONFIG > $MM_CONFIG.tmp && mv $MM_CONFIG.tmp $MM_CONFIG
-
-    # Configure database access
-    if [[ -z "$MM_SQLSETTINGS_DATASOURCE" ]]
-    then
-      echo -ne "Configure database connection..."
-      # URLEncode the password, allowing for special characters
-      ENCODED_PASSWORD=$(printf %s $MM_PASSWORD | jq -s -R -r @uri)
-      export MM_SQLSETTINGS_DATASOURCE="postgres://$MM_USERNAME:$ENCODED_PASSWORD@$DB_HOST:$DB_PORT_NUMBER/$MM_DBNAME?sslmode=disable&connect_timeout=10"
-      echo OK
-    else
-      echo "Using existing database connection"
-    fi
   else
     echo "Using existing config file" $MM_CONFIG
+  fi
+
+  # Configure database access
+  if [[ -z "$MM_SQLSETTINGS_DATASOURCE" && ! -z "$MM_USERNAME" && ! -z "$MM_PASSWORD" ]]
+  then
+    echo -ne "Configure database connection..."
+    # URLEncode the password, allowing for special characters
+    ENCODED_PASSWORD=$(printf %s $MM_PASSWORD | jq -s -R -r @uri)
+    export MM_SQLSETTINGS_DATASOURCE="postgres://$MM_USERNAME:$ENCODED_PASSWORD@$DB_HOST:$DB_PORT_NUMBER/$MM_DBNAME?sslmode=disable&connect_timeout=10"
+    echo OK
+  else
+    echo "Using existing database connection"
   fi
 
   # Wait another second for the database to be properly started.
