d84a094bd15e76d92a4b9b84549a8a3659ac4617
cobenash <victor.yang@hellosanta.com.tw>
2018-10-29 08:21:46 +0800

    remove script at wal-e script

--- db/setup-wale.sh
+++ db/setup-wale.sh
@@ -5,9 +5,3 @@ echo "wal_level = $WAL_LEVEL" >> $PGDATA/postgresql.conf
 echo "archive_mode = $ARCHIVE_MODE" >> $PGDATA/postgresql.conf
 echo "archive_command = '/usr/bin/wal-e wal-push %p'" >> $PGDATA/postgresql.conf
 echo "archive_timeout = $ARCHIVE_TIMEOUT" >> $PGDATA/postgresql.conf
-
-# envdir specific configuration setup
-echo $AWS_ACCESS_KEY_ID > /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY > /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
-echo $WALE_S3_PREFIX > /etc/wal-e.d/env/WALE_S3_PREFIX
-echo $AWS_REGION > /etc/wal-e.d/env/AWS_REGION
